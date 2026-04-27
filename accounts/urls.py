aea4e54dc9eae796d83371e7537f548f269bb8e6
Rishil-sim04 <rishil.kathiriya@simformsolutions.com>
2026-04-27 11:36:44 +0530

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,13 +1,16 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import RegisterView, PasswordChangeView, LogoutView
+from .views import RegisterView, PasswordChangeView, LogoutView, UpdateProfileView, EmployeeListView,
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', TokenObtainPairView.as_view(), name='login'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('password-change/', PasswordChangeView.as_view(), name='password_change'),
-    path('logout/', LogoutView.as_view(), name='logout')
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('profile/', UpdateProfileView.as_view(), name='update_own_profile'),
+    path('profile/<int:emp_id>/', UpdateProfileView.as_view(), name='update_emp_profile'),
+    path('employees/', EmployeeListView.as_view(), name='employee_list'),
 ]
  
 
